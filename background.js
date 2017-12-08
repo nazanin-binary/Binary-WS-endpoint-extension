72f3ed00f61aab36ac3467e2b47eb5d80a1982fe
nazaninreihani <nazanin.reyh@gmail.com>
2017-12-08 13:22:38 +0330

    init extension

---
+++ background.js
@@ -0,0 +1,27 @@
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(() => {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a '.binary.com' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlMatches: '(staging|www|app|ticktrade|developers|webtrader)\.binary.com' },
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: '.github.io/binary-static/' },
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'localhost' },
+          }),
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
+const select = element => document.querySelector(element);
