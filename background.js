7224128079424ae6ad6153db0b19db2004d1f08c
nazaninreihani <nazanin.reyh@gmail.com>
2018-06-06 16:36:53 +0430

    Add binary.sx domain

--- background.js
+++ background.js
@@ -13,6 +13,9 @@ chrome.runtime.onInstalled.addListener(() => {
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: { urlMatches: '.github.io/binary-(static|bot)/' },
           }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlMatches: '.binary.sx' },
+          }),
         ],
         // And shows the extension's page action.
         actions: [ new chrome.declarativeContent.ShowPageAction() ]
